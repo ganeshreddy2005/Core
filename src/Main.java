0c3154458f37da288668c05ea59a71d63f2a6820
bijay <bijaydhm@gmail.com>
2020-08-19 20:52:04 -0500

    I have changed on Aug19

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Test class");
         System.out.println("Today");
         System.out.println("August_17");
+        System.out.println("Again changes on August_17");
         try {
             Card card=Card.class.newInstance();
         } catch (InstantiationException e) {
@@ -14,5 +15,8 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println("Main Class End");
+        System.out.println("Aug_17");
+        System.out.println("Aug_18");
+        System.out.println("Aug_19");
     }
 }
